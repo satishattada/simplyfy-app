b17cbfe518d4b4e97883afcb892a7da82f821c64
satish.attada <satish.attada@bp.com>
2024-12-18 17:43:54 +0000

    updated

--- src/pages/layout/demandManagement/demandManagement.tsx
+++ src/pages/layout/demandManagement/demandManagement.tsx
@@ -1,6 +1,10 @@
 
+import { useAtomValue } from 'jotai';
 import './styles.css';
+import { configAtom } from '../../../providers/store';
 
 export default function DemandManagement() {
+  const config = useAtomValue(configAtom);
+  console.log(config)
   return <div className="home-container container">DemandManagement</div>;
 }

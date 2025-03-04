47b73d9ec490cd9d65eb0ea2c5b5ee87cfa6493a
PallaviB07 <pallavi.bhadange@gmail.com>
2025-03-04 20:01:46 +0530

    seperate fill added for fields array

--- src/components/contractManagementModal/contractManagementModal.jsx
+++ src/components/contractManagementModal/contractManagementModal.jsx
@@ -7,6 +7,7 @@ import { useAtom } from "jotai";
 import { contractDataAtom } from "../../atoms/contractAtoms";
 import { nodeModuleNameResolver } from "typescript";
 import selectOptions from "../../data/dropDown";
+import bpReportingFields from "../../data/reportingField";
 import Table from "react-bootstrap/Table";
 import { FaTrash } from "react-icons/fa";
 
@@ -84,7 +85,10 @@ const ContractManagementModal = ({
         formData && formData?.milestoneAmount.map((item) => item.year)
       ),
     ];
-   setSelectedYear(years[0]);
+    if(modalType === "add" || selectedYear === null || selectedYear === undefined){
+      setSelectedYear(years[0]);
+    }
+    
   }, [formData]);
   useEffect(() => {
     const filteredData = formData?.milestoneAmount.filter(
@@ -367,73 +371,7 @@ const handleTabClick = (year) => {
     onClose();
   };
 
-  const bpReportingFields = [
-    {
-      label: "BP Sub Portfolio",
-      name: "bpSubPortfolio",
-      fieldType: "dropdown",
-    },
-    { label: "Contract Name", name: "contractName", fieldType: "text" },
-    { label: "Contract Type", name: "contractType", fieldType: "dropdown" },
-    {
-      label: "Discount Percentage",
-      name: "discountPercentage",
-      fieldType: "text",
-    },
-    { label: "Team Type", name: "teamType", fieldType: "dropdown" },
-    {
-      label: "Contract Currency",
-      name: "contractCurrency",
-      fieldType: "dropdown",
-    },
-    { label: "Contract FGID", name: "contractFGID", fieldType: "text" },
-    { label: "PO Revision", name: "PORevision", fieldType: "dropdown" },
-
-    {
-      label: "Contract Program (Mission / Program)",
-      name: "contractProgram",
-      fieldType: "dropdown",
-    },
-
-    { label: "Contract CSG", name: "contractCSG", fieldType: "text" },
-    { label: "Revenue Type", name: "revenueType", fieldType: "dropdown" },
-    {
-      label: "Contract Start Date",
-      name: "contractStartDate",
-      fieldType: "text",
-    },
-    { label: "Contract End Date", name: "contractEndDate", fieldType: "text" },
-    {
-      label: "Contract Currency",
-      name: "contractCurrency",
-      fieldType: "dropdown",
-    },
-    { label: "Reference PO", name: "referencePO", fieldType: "text" },
-    { label: "PO Amount OMS", name: "POAmountOMS", fieldType: "text" },
-    { label: "PO Amount FG", name: "POAmountFG", fieldType: "text" },
-    { label: "PO Amount Ariba", name: "POAmountAriba", fieldType: "text" },
-    {
-      label: "Master Project Code",
-      name: "masterProjectCode",
-      fieldType: "text",
-    },
-    {
-      label: "Master Project Code PM",
-      name: "masterProjectCodePM",
-      fieldType: "text",
-    },
-    { label: "Master PU", name: "masterPU", fieldType: "text" },
-    { label: "LOE Number", name: "LOENumber", fieldType: "text" },
-    { label: "Linked DPS Number", name: "linkedDPSNumber", fieldType: "text" },
-    {
-      label: "Infosys Contract Type",
-      name: "infosysContractType",
-      fieldType: "dropdown",
-    },
-    { label: "Total SoW Workers", name: "totalSoWWorkers", fieldType: "text" },
-  ];
- 
-  const addNewRevisionYear =(e)=>{
+   const addNewRevisionYear =(e)=>{
     setFormData((prevData) => {
       const updatedMilestoneAmount = [{
         revision: selectedYearData?.length <=1 ? 0 : selectedYearData?.length,
---
+++ src/data/reportingField.js
@@ -0,0 +1,67 @@
+const bpReportingFields = [
+    {
+      label: "BP Sub Portfolio",
+      name: "bpSubPortfolio",
+      fieldType: "dropdown",
+    },
+    { label: "Contract Name", name: "contractName", fieldType: "text" },
+    { label: "Contract Type", name: "contractType", fieldType: "dropdown" },
+    {
+      label: "Discount Percentage",
+      name: "discountPercentage",
+      fieldType: "text",
+    },
+    { label: "Team Type", name: "teamType", fieldType: "dropdown" },
+    {
+      label: "Contract Currency",
+      name: "contractCurrency",
+      fieldType: "dropdown",
+    },
+    { label: "Contract FGID", name: "contractFGID", fieldType: "text" },
+    { label: "PO Revision", name: "PORevision", fieldType: "dropdown" },
+
+    {
+      label: "Contract Program (Mission / Program)",
+      name: "contractProgram",
+      fieldType: "dropdown",
+    },
+
+    { label: "Contract CSG", name: "contractCSG", fieldType: "text" },
+    { label: "Revenue Type", name: "revenueType", fieldType: "dropdown" },
+    {
+      label: "Contract Start Date",
+      name: "contractStartDate",
+      fieldType: "text",
+    },
+    { label: "Contract End Date", name: "contractEndDate", fieldType: "text" },
+    {
+      label: "Contract Currency",
+      name: "contractCurrency",
+      fieldType: "dropdown",
+    },
+    { label: "Reference PO", name: "referencePO", fieldType: "text" },
+    { label: "PO Amount OMS", name: "POAmountOMS", fieldType: "text" },
+    { label: "PO Amount FG", name: "POAmountFG", fieldType: "text" },
+    { label: "PO Amount Ariba", name: "POAmountAriba", fieldType: "text" },
+    {
+      label: "Master Project Code",
+      name: "masterProjectCode",
+      fieldType: "text",
+    },
+    {
+      label: "Master Project Code PM",
+      name: "masterProjectCodePM",
+      fieldType: "text",
+    },
+    { label: "Master PU", name: "masterPU", fieldType: "text" },
+    { label: "LOE Number", name: "LOENumber", fieldType: "text" },
+    { label: "Linked DPS Number", name: "linkedDPSNumber", fieldType: "text" },
+    {
+      label: "Infosys Contract Type",
+      name: "infosysContractType",
+      fieldType: "dropdown",
+    },
+    { label: "Total SoW Workers", name: "totalSoWWorkers", fieldType: "text" },
+  ];
+
+  export default bpReportingFields;
